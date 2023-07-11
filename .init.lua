7933f1b1f997a848733449ab927a35c4c121f18b
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2023-07-11 23:00:25 +0200

    Cleanup

--- .init.lua
+++ .init.lua
@@ -1,160 +1,133 @@
---package.path=package.path..";./.lua/?.lua"
-
---local def = {}
---for k in pairs(package.loaded) do def[k] = true end
---
---function OnHttpRequest()
---  -- clear package cache
---  for k in pairs(package.loaded) do
---    if not def[k] then package.loaded[k] = nil end
---  end
---  Route()
---end
 DB_FILE ="/tmp/out.db"
 COOKIE_KEY="advent_session"
 
-os.remove(DB_FILE)
-os.remove(DB_FILE.."-wal")
-os.remove(DB_FILE.."-shm")
+local _db
+db = {}
 
-
-local db = require "db"
-
---TODO: db is locked whenever not using wal mode
--- probably bad optimization, keeping db handles and or
--- statement handles around while not needed
--- TODO: use rowid instead of id autoincrement
-db.exec [[
-PRAGMA journal_mode=wal;
-PRAGMA synchronous=normal;
-
-CREATE TABLE IF NOT EXISTS user (
-  id    INTEGER NOT NULL UNIQUE DEFAULT 0,
-  name  TEXT NOT NULL,
-  link  TEXT,
-  --score INTEGER NOT NULL DEFAULT 0,
-  PRIMARY KEY (id AUTOINCREMENT)
-);
-
-CREATE TABLE IF NOT EXISTS session (
-  user_id INTEGER NOT NULL UNIQUE,
-  token TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS achievement (
-  user_id   INTEGER NOT NULL,
-  puzzle_id INTEGER NOT NULL,
-  time      INTEGER NOT NULL, -- unix time
-  type      TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS puzzle (
-  id   INTEGER NOT NULL UNIQUE DEFAULT 0,
-  name TEXT NOT NULL UNIQUE,
-  time_start REAL NOT NULL, -- unix time
-  html TEXT NOT NULL,
-  gen_code TEXT NOT NULL,
-  PRIMARY KEY (id AUTOINCREMENT)
-);
-
-CREATE TABLE IF NOT EXISTS user_puzzle(
-  user_id   INTEGER NOT NULL,
-  puzzle_id INTEGER NOT NULL,
-  bucket_id INTEGER NOT NULL,
-  last_try INTEGER, -- unix time
-  next_try INTEGER, -- unix time maybe we don't need the previous
-  silver_time REAL,
-  gold_time REAL,
-  fails INTEGER NOT NULL DEFAULT 0
-);
-
-CREATE TABLE IF NOT EXISTS bucket(
-  id INTEGER NOT NULL UNIQUE DEFAULT 0,
-  puzzle_id INTEGER NOT NULL,
-  input     BLOB NOT NULL,
-  silver_answer    BLOB NOT NULL,
-  gold_answer,
-  PRIMARY KEY (id AUTOINCREMENT)
-);
-
-CREATE TABLE IF NOT EXISTS leaderboard (
-  score INTEGER NOT NULL DEFAULT 0,
-  user_id INTEGER NOT NULL
-);
-
---CREATE TRIGGER IF NOT EXISTS update_score
---  AFTER INSERT
---  ON achievement
---BEGIN
---  UPDATE user SET score = score + 1 WHERE id = NEW.user_id;
---END;
-]]
-
-for i=1,30 do
-  local link
-  local name = "Joe" .. i
-  if i % 2 == 0 then
-    link = "https://github.com/" .. name
-  end
-  db.urow("INSERT INTO user(name, link) VALUES (?, ?)", name, link)
+local function prep(sql, ...)
+  local stmt = _db:prepare(sql)
+  if not stmt then error(_db:errmsg()) end
+  assert(stmt:bind_values(...) == lsqlite3.OK)
+  return stmt
 end
 
-db.urow([[
-INSERT INTO puzzle (name, time_start, html, gen_code) VALUES (
-  '01', ?, '
-<p>Add one to input</p>
-<em>Example:</em>
-<pre>
-9
-</pre>
-<p>The answer would be <code>10</code>.</p>
-$PART2$
-<p>Add two to input</p>
-<p>With the same input as before, the answer would be <code>10</code>.</p>
-  ', '
-  local n = math.random(998)
-  return n, n + 1, n + 2
-'), (
-  '02', ?, '
-<p>Multiply by four</p>
-<em>Example:</em>
-<pre>
-9
-</pre>
-<p>The answer would be <code>36</code>.</p>
-$PART2$
-<p>Multiply by 5</p>
-<p>With the same input as before, the answer would be <code>45</code>.</p>
-  ', '
-  local n = math.random(100, 249)
-  return n, n * 4, n * 5
-');]], GetTime(), GetTime() + 10)
+function db.exec(sql)
+  local ret = _db:exec(sql)
+  if ret ~= lsqlite3.OK then error(_db:errmsg()) end
+end
 
+function db.urow(sql, ...)
+  local stmt = prep(sql, ...)
+  return stmt:urows()(stmt)
+end
 
-package.loaded.db = nil -- MAKE SURE WORKERS DON'T HAVE DB HANDLE FROM HERE
-db = nil
-collectgarbage()
+function db.urows(sql, ...)
+  local stmt = prep(sql, ...)
+  return stmt:urows(), stmt
+end
+
+function db.transaction(f)
+  db.exec"BEGIN TRANSACTION;"
+
+  local ok, err = pcall(f)
+  if not ok then
+    db.exec"ROLLBACK;"
+    error(err)
+  end
+
+  db.exec"COMMIT;"
+end
+
+local curtoken, curuser
+function db.get_session_user_id() -- simply cached
+  local token = GetCookie(COOKIE_KEY)
+  if not token then return nil end
+  if token == curtoken then return curuser end
+  curtoken, curuser = token, db.urow("SELECT user_id FROM session WHERE token = ?", token)
+  return curuser
+end
+
+local function fill_bucket(puzzle, amount)
+  local code = db.urow([[
+  SELECT gen_code FROM puzzle WHERE name = ?
+  ]], puzzle)
+  local f = assert(load(code))
+
+  local entropy = os.time()
+
+  for i = 1, amount do
+    math.randomseed(entropy+i)
+    local input, silver, gold = f()
+    assert(input and silver)
+    db.urow([[
+    INSERT INTO bucket(puzzle, input, silver_answer, gold_answer) VALUES (?, ?, ?, ?)
+    ]], puzzle, input, silver, gold)
+  end
+end
+
+function db.get_user_bucket(user_id, puzzle)
+  local bucket = db.urow([[
+  SELECT bucket_id
+  FROM user_puzzle
+  WHERE puzzle = ?
+    AND user_id = ?
+  ]], puzzle, user_id)
+
+  if not bucket then
+    --TODO: instead of random bucket, maybe ring loop index over bucket
+    ::randombucket::
+    bucket = db.urow([[
+    SELECT rowid
+    FROM bucket
+    WHERE puzzle = ?
+    ORDER BY RANDOM()
+    LIMIT 1;
+    ]], puzzle)
+    if not bucket then
+      fill_bucket(puzzle, 10)
+      goto randombucket
+    end
+    db.urow([[
+    INSERT INTO user_puzzle(user_id, puzzle, bucket_id)
+    VALUES (?, ?, ?)
+    ]], user_id, puzzle, bucket)
+  end
+  return bucket
+end
+
+local function open_db()
+  _db = lsqlite3.open(DB_FILE)
+  _db:busy_timeout(1000)
+  db.exec[[
+  PRAGMA journal_mode=wal;
+  PRAGMA synchronous=normal;
+  ]]
+end
+
+function OnWorkerStart()
+  open_db()
+end
 
 function OnServerHeartbeat()
-  db = require"db"
-
   --TODO: probably horribly inefficient
+  --TODO: fork here? permissions?
+  open_db()
   db.transaction(function()
     local users = {}
     db.exec[[DELETE FROM leaderboard]]
-    for puzzle_id in db.urows[[
-      SELECT id
+    for name in db.urows[[
+      SELECT name
       FROM puzzle
       ]] do
       local score = 100
       for user_id in db.urows([[
         SELECT user_id
         FROM user_puzzle
-        WHERE puzzle_id = ?
+        WHERE puzzle = ?
         AND silver_time IS NOT NULL
         ORDER BY silver_time
         LIMIT 100
-        ]], puzzle_id) do
+        ]], name) do
         users[user_id]=(users[user_id] or 0) + score
         score=score-1
       end
@@ -166,6 +139,71 @@ function OnServerHeartbeat()
       ]], user_id, score)
     end
   end)
+  _db:close()
 end
 
-ProgramHeartbeatInterval(10 * 1000) -- 10s
+ProgramHeartbeatInterval(3 * 1000) -- 10s
+
+wrt, fmt, esc = Write, string.format, EscapeHtml
+
+html = {}
+
+function html.page_begin(title)
+  wrt[[<!DOCTYPE html>]]
+  wrt[[<html lang="en">]]
+  wrt[[<meta charset="UTF-8">]]
+  wrt[[<link rel="stylesheet" href="/style.css">]]
+  wrt[[<nav>]]
+  wrt[[<a href="/">Advent</a>]]
+  wrt" | "
+  wrt[[<a href="/about">About</a>]]
+  wrt" | "
+  wrt[[<a href="/leaderboard">Leaderboard</a>]]
+  wrt" | "
+  local user_id = db.get_session_user_id()
+  local name
+  if user_id then
+    name = db.urow("SELECT name FROM user WHERE rowid = ?", user_id)
+  end
+  wrt(fmt([[<a href="/profile">%s</a>]], name and EscapeHtml(name) or "Login"))
+
+  --html.maybelink("Leaderboard", p ~= "/leaderboard.lua" and "/leaderboard.lua")
+  wrt[[</nav>]]
+  wrt"<main>"
+end
+
+function html.page_end()
+  wrt"</main>"
+  wrt[[</html>]]
+end
+
+function html.leaderboard_begin()
+  html.page_begin()
+
+  wrt"<p>Per day:"
+  for name in db.urows"SELECT name FROM puzzle ORDER BY time_start" do
+    local link = name
+    if name == puzzle then link = "<strong>"..name.."</strong>" end
+    wrt(fmt([[ <a href="/%s/leaderboard">%s</a>]], link, name))
+  end
+  wrt"</p>"
+end
+
+function OnHttpRequest()
+  local p = GetPath()
+
+  if p == "/style.css" then return ServeAsset("/style.css") end
+
+  local cmd
+
+  puzzle, cmd = p:match"^/(%d%d?)/?(%l*)$"
+  if not cmd then cmd = p:match"^/(%l*)$" end
+  if not cmd then return ServeError(404) end
+  if cmd == "" then cmd = "index" end
+  if puzzle then cmd = "puzzle-" .. cmd end
+
+  --print("Access", "/"..cmd..".lua")
+
+  return RoutePath("/"..cmd..".lua")
+end
+
